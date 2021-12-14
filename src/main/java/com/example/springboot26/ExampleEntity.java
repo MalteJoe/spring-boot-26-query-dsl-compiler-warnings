eb529c4bd7ae3d1f56f0f83929890db6594b61d0
Malte Jörgens <malte.joergens@wps.de>
2021-12-14 16:09:31 +0100

    Initial commit

---
+++ src/main/java/com/example/springboot26/ExampleEntity.java
@@ -0,0 +1,10 @@
+package com.example.springboot26;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ExampleEntity {
+    @Id
+    Long id;
+}
---
+++ src/main/java/com/example/springboot26/SpringBoot26Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot26Application.class, args);
+    }
+
+}
